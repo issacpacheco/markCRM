3f3101a6b9f30aaa13959e9c1098cca20938a2ae
Issac Pacheco <isaacpacheco.go@gmail.com>
2022-08-08 11:12:36 -0500

    se inicia clase de ventas

--- admin/class/ventas.class.php
+++ admin/class/ventas.class.php
@@ -3,7 +3,9 @@ namespace nsventas;
 use conexionbd\mysqlconsultas;
 
 class ventas extends mysqlconsultas{
-
+    public function prospectos(){
+        $qry = "SELECT * FROM prospectos";
+    }
 }
 
 
